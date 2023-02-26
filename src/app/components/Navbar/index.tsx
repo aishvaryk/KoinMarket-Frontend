a45c7615b8b1ab98f7734d377dc294b7948c5e97
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2023-02-26 12:40:26 +0530

    styled navbar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,11 +2,12 @@
 import { Box, Typography } from "@mui/material";
 import { FlexBox } from "../global/FlexBox";
 import { Logo } from "../Logo";
+import "./navbar.css";
 
 export function Navbar() {
   return (
     <FlexBox className={"navbar-wrapper"} height={"60px"} width={"100%"}>
-      <Box sx={{ height: "100%", width: "110px", position: "relative" }}>
+      <Box sx={{ height: "40px", width: "110px", position: "relative" }}>
         <Logo></Logo>
       </Box>
     </FlexBox>
