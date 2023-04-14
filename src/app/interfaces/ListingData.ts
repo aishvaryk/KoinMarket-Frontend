3a881be905e7d81bb0733cbd0e4c958d1d7fc4d6
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2023-04-14 20:06:50 +0530

    Added some useful interfaces

---
+++ src/app/interfaces/ListingData.ts
@@ -0,0 +1,11 @@
+export interface ListingData {
+    id: number;
+    name: string;
+    logoURL: string;
+    symbol: string;
+    marketCap: number;
+    price: number;
+    circulatingSupply: number;
+    change24h: number;
+    change1d: number;
+  }
