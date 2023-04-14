f59ec3907b930fd292c645954539c22f1d8534c5
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2023-04-14 20:07:23 +0530

    Created static table for home page

--- src/app/components/CryptoTable/index.tsx
+++ src/app/components/CryptoTable/index.tsx
@@ -1,27 +1,25 @@
 "use client"
 
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { FlexBox } from '../global/FlexBox';
 import { useTableRows } from './store';
 
 export function CryptoTable() {
-  useTableRows();
-
-
-
-
+  
   return (
-    <TableContainer component={Paper}>
-     <Table aria-label="simple table">
-       <TableHead>
-         <TableRow>
-           <TableCell>S.No</TableCell>
-           <TableCell align="right">Item</TableCell>
-           <TableCell align="right">Quantity&nbsp;(kg)</TableCell>
-           <TableCell align="right">Price&nbsp;($)</TableCell>
-         </TableRow>
-       </TableHead>
-       <TableBody>
-         {/* {rows.map((row) => (
+    <FlexBox justify="center" height="auto" customStyle={{margin:"20px"}}>
+      <TableContainer component={Paper} sx={{ width: "80%" }}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>S.No</TableCell>
+              <TableCell align="right">Item</TableCell>
+              <TableCell align="right">Quantity&nbsp;(kg)</TableCell>
+              <TableCell align="right">Price&nbsp;($)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* {rows.map((row) => (
            <TableRow key={row.number}>
              <TableCell component="th" scope="row">
                {row.number}
@@ -31,8 +29,9 @@ export function CryptoTable() {
              <TableCell align="right">{row.price}</TableCell>
            </TableRow>
          ))} */}
-       </TableBody>
-     </Table>
-   </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </FlexBox>
   );
 }
--- src/app/interfaces/TableRowData.ts
+++
@@ -1,6 +0,0 @@
-export interface TableRowData {
-    name:string,
-    logoURL: string,
-    symbol: string,
-     marketCap: number, price: number, volume: number, circulatingSupply: number, change: number
-}
