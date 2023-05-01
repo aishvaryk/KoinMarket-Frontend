fc2b62272599305a9e26da94fd377dda04b5ebc8
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2023-05-01 15:28:57 +0530

    Add option to add a coin to a watchlist

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,14 +2,17 @@
 
 import axios from "axios";
 import Image from "next/image";
-import { useEffect, useRef, useState, FC } from "react";
-import { Box, LinearProgress, Typography, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, LinearProgress, Typography, Paper, MenuItem, IconButton, Menu } from "@mui/material";
 import { BASE_URL } from "../constants/backendURL";
 import TradingViewChartWidget from "../components/TradingView/Chart";
 import { MetaData } from "../interfaces/MetaData";
 import { Language, Twitter, Reddit } from "@mui/icons-material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { Watchlist } from "../interfaces/Watchlist";
+import { useUser } from "../user/store";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export default function CoinDetails({ params }: { params: { id: number } }) {
   const router = useRouter();
@@ -23,6 +26,11 @@ export default function CoinDetails({ params }: { params: { id: number } }) {
     twitter: "#",
     reddit: "#",
   });
+  const [watchlists, setWatchlists] = useState<Watchlist[]>([]);
+  const { user, setUser } = useUser();
+  const [anchorElMenu, setAnchorElMenu] = useState<null | HTMLElement>(
+    null
+  );
 
   useEffect(() => {
     setIsLoading(true);
@@ -55,6 +63,63 @@ export default function CoinDetails({ params }: { params: { id: number } }) {
       });
   }, []);
 
+  useEffect(() => {
+    if (user) {
+      axios({
+        url: BASE_URL + "watchlist/all",
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + user?.token,
+        },
+        withCredentials: false,
+      })
+        .then((res) => {
+          return res.data.map((watchlist: any): Watchlist => {
+            return {
+              id: watchlist.id,
+              name: watchlist.name,
+              list: watchlist.listing,
+            };
+          });
+        })
+        .then((watchlists: Watchlist[]) => {
+          setWatchlists(watchlists);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [user, watchlists.length]);
+
+  const handleOpenWatchlistMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElMenu(event.currentTarget);
+  };
+
+  const handleCloseWatchlistMenu = () => {
+    setAnchorElMenu(null);
+  };
+
+  const handleMenuItemClick = (watchlistId: number)  => { 
+    axios({
+      url: BASE_URL + "watchlist/" + watchlistId + "/add",
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + user?.token,
+      },
+      params: {
+        coinId: params.id
+      },
+      withCredentials: false,
+    })
+      .then((res) => {
+        console.log("added");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
   if (isLoading) {
     return <LinearProgress />;
   } else {
@@ -79,20 +144,56 @@ export default function CoinDetails({ params }: { params: { id: number } }) {
           <Paper elevation={1} sx={{ display: "flex", alignItems: "center" }}>
             <Typography variant="h4">{metadata.symbol}</Typography>
           </Paper>
-          <Link
-            href={metadata.twitter}
-            style={{ display: metadata.twitter === "#" ? "none" : "inline" }}
-          >
-            <Twitter />
-          </Link>
-          <Link href={metadata.reddit}>
-            <Reddit />
-          </Link>
-          <Link href={metadata.website}>
-            <Language />
-          </Link>
+          {metadata.twitter === "#" ? (
+            <></>
+          ) : (
+            <Link href={metadata.twitter} style={{ color: "#00acee " }}>
+              <Twitter />
+            </Link>
+          )}
+          {metadata.reddit === "#" ? (
+            <></>
+          ) : (
+            <Link href={metadata.reddit} style={{ color: "##ff4500 " }}>
+              <Reddit />
+            </Link>
+          )}
+          {metadata.website === "#" ? (
+            <></>
+          ) : (
+            <Link href={metadata.website} style={{ color: "blue" }}>
+              <Language />
+            </Link>
+          )}
         </Box>
         <Typography variant="body1">{metadata.description}</Typography>
+        {(watchlists.length===0 || user) ? <Box>
+            <IconButton onClick={handleOpenWatchlistMenu} sx={{ p: 0 }}>
+               <MoreVertIcon />
+            </IconButton>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElMenu}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElMenu)}
+              onClose={handleCloseWatchlistMenu}
+            >
+              {watchlists.map((watchlist) => (
+                  <MenuItem key={watchlist.id} onClick={()=>handleMenuItemClick(watchlist.id)}>
+                    <Typography textAlign="center">{watchlist.name}</Typography>
+                  </MenuItem>
+              ))}
+            </Menu>
+          </Box> : <></>}
         <Box
           sx={{
             width: { xs: "95%", sm: "85%", md: "85%" },
